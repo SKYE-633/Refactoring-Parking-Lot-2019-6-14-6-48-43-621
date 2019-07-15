1d8292bf15c4f0a1fc298c963296804f8be3f850
homework <52529600+SKYE-633@users.noreply.github.com>
2019-07-15 21:12:24 +0800

    Update Ticket.java

--- ParkingLot—New/src/main/java/com/thoughtworks/tdd/Ticket.java
+++ ParkingLot—New/src/main/java/com/thoughtworks/tdd/Ticket.java
@@ -1,17 +1,5 @@
 package com.thoughtworks.tdd;
 
-public class Ticket {
-    private boolean isUsed;
-    public void useTicket() {
-        isUsed = true;
-    }
 
-    public boolean isUsed() {
-        return isUsed;
-    }
-
-    public Ticket(Car car) {
-        isUsed = false;
-    }
 
 }
