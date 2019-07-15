8ff43d96c0336f20b0681d45a38fb6029ee773b5
homework <52529600+SKYE-633@users.noreply.github.com>
2019-07-15 21:13:17 +0800

    [$ Skye $] refactor：$ code smell $ in Ticket class Extract variable。
    [$ Skye $] refactor：$ code smell $ in Ticket class Extract variable。

--- ParkingLot—New/src/main/java/com/thoughtworks/tdd/Ticket.java
+++ ParkingLot—New/src/main/java/com/thoughtworks/tdd/Ticket.java
@@ -1,5 +1,16 @@
 package com.thoughtworks.tdd;
 
+public class Ticket {
+    private boolean isUsed;
+    public void useTicket() {
+        isUsed = true;
+    }
 
+    public boolean isUsed() {
+        return isUsed;
+    }
 
+    public Ticket(Car car) {
+        isUsed = false;
+    }
 }
